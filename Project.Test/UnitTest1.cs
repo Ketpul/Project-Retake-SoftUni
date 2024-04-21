213c02eca7d4a6629da8438b6ff888c151314e43
Ketpul <emiliqnnik07@abv.bg>
2024-04-21 16:37:28 +0300

    NUnitTest

---
+++ Project.Test/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace Project.Test
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
